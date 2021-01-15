11c03dfe78bd3d13585741fd0f71d58ce84f2ad7
moinul <mdmoinulh13@gmail.com>
2021-01-15 16:56:03 +0600

    maximum subarray added

---
+++ Codes/53. Maximum Subarray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    
+    let maxSum=nums[0];
+    let currentSum=nums[0];
+    
+    for(let i=1;i<nums.length;i++){
+        currentSum=Math.max(currentSum+nums[i],nums[i]);
+        maxSum=Math.max(maxSum,currentSum);
+    }
+    
+    return maxSum;
+};
