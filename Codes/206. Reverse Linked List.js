87c7d2061e387ec25d1159c69d71377b85bc13f6
moinul <1405109.mmh@ugrad.cse.buet.ac.bd>
2021-01-18 23:42:31 +0600

    reverse linkedlist added

---
+++ Codes/206. Reverse Linked List.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let temp=head;
+    let prev=null;
+    
+    while(temp!=null){
+       let t=temp.next;
+        temp.next=prev;
+        prev=temp;
+        temp=t;
+    }
+    
+    return prev;
+};
